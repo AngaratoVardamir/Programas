089c10904ca31b33438c2b39fe19ed480947af8c
Gustavo Augusto <141056143+AngaratoVardamir@users.noreply.github.com>
2024-02-27 21:25:36 -0300

    Up 01py > js

--- Desafios Py ➡️ Js/Desafio_01/Script.js
+++
@@ -1,4 +0,0 @@
-function Click(){
-    var Nome = window.prompt('Qual é o seu nome?')
-    Texto.innerHTML = `<h4> Olá ${Nome} Prazer em te Conhecer! </h4>`
-}
---
+++ Desafios Py ➡️ Js/Desafio_02/Script.js
@@ -0,0 +1,7 @@
+function Clicando(){
+    var data = window.document.getElementById('Data')
+
+    var n1 = Number(data.value)
+    console.log(n1)
+    resizeBy.innerHTML = `Soma ${n1}`
+}
